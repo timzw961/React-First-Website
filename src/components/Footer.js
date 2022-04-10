5998df9205570751b45ac49d8ddfc53c124a1f08
Timothy <102215468+timzw961@users.noreply.github.com>
2022-04-10 20:03:12 +1000

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ export default function Footer(){
     return (
         <footer>
             <small> Website developed by Timothy Wang </small>
+            <small> This is a small test to see if GitHub Desktop works </small>
         </footer>
     )
 }
